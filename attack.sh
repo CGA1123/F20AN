7e8f50ae03c3e5581d7147b9c4c7e1ba858cfbe2
Christian Gregg <c_arlt@hotmail.com>
2018-02-23 18:46:17 +0000

    Create torrent file, finish attacker install script

---
+++ attack.sh
@@ -0,0 +1,1 @@
+echo "Attack Successful!"
--- attacker-install.sh
+++ attacker-install.sh
@@ -17,12 +17,6 @@ EOF
 
 read -n 1 -s
 
-echo "-> Installing required dependencies"
-sudo apt -qq update
-sudo apt -y -qq install \
-	qbittorrent \
-	net-tools
-
 echo "-> Setting IP to 10.0.2.30"
 sudo tee -a /etc/network/interfaces > /dev/null <<EOF
 
@@ -35,17 +29,14 @@ EOF
 
 sudo ifup enp0s3
 
-# TODO:
-# Need to set up qbittorrent to server torrent file...
-
 echo "-> Starting webserver on port 80"
 echo "->	(This will server the attack 'landing' page)"
 echo "->	(output logged to ./http.server.80.log)"
-sudo python3 -m http.server 80 > http.server.80.log 2>&1 &
+sudo python3 -m http.server 80 > ${HOME}/F20AN/http.server.80.log 2>&1 &
 
 echo "-> Starting webserver on port 9091"
 echo "->	(This will serve the attack payload and run the exploit)"
 echo "->	(output logged to ./http.server.9091.log)"
-python3 -m http.server 9091 > http.server.9091.log 2>&1 &
+python3 -m http.server 9091 > ${HOME}/F20AN/http.server.9091.log 2>&1 &
 
 echo "-> DONE!"
