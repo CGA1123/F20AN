3dc4700fcc81f40dd75b1b4d1f60ca748f8f462b
Christian Gregg <c_arlt@hotmail.com>
2018-02-24 12:52:44 +0000

    Add message about server the attack.sh file from http://10.0.2.30:80

--- attacker-install.sh
+++ attacker-install.sh
@@ -29,6 +29,17 @@ EOF
 
 sudo ifup enp0s3
 
+cat <<EOF
+-> The attack script will be requested from http://10.0.2.30/attack.sh
+-> You need to serve that file for this attack to be successful
+-> A `attack.sh` file is provided in the git project root
+-> Running `sudo python3 -m http.server 80` will serve this file.
+
+-> If you want to get the attack file from a different location you will need
+-> to create a new `.profile.torrent` file and update the attack payload
+-> to run that torrent file.
+EOF
+
 echo "-> Starting webserver on port 9091"
 echo "->	(This will serve the attack payload and run the exploit)"
 echo "->	(output logged to ./http.server.9091.log)"
